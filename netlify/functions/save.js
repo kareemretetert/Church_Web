80d27cdf00d3e4fbaab31d8e1076a303ace88db6
Kareem Hosny <hsnykrym27@gmail.com>
2026-03-16 21:09:03 +0200

    Update save.js

--- netlify/functions/save.js
+++ netlify/functions/save.js
@@ -1,28 +1,38 @@
-exports.handler = async (event) => {
+exports.handler = async function(event) {
 
-const fs = require("fs")
-const path = require("path")
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 
-try {
+const repo = "kareemretetert/Church_Web"
+const path = "summaries.json"
 
 const data = JSON.parse(event.body)
 
-const filePath = path.join(process.cwd(), "summaries.json")
+const fileRes = await fetch(`https://api.github.com/repos/${repo}/contents/${path}`,{
+headers:{
+Authorization:`token ${GITHUB_TOKEN}`
+}
+})
 
-fs.writeFileSync(filePath, JSON.stringify(data,null,2))
+const file = await fileRes.json()
+
+const updatedContent = Buffer.from(JSON.stringify(data,null,2)).toString("base64")
+
+await fetch(`https://api.github.com/repos/${repo}/contents/${path}`,{
+method:"PUT",
+headers:{
+Authorization:`token ${GITHUB_TOKEN}`,
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+message:"update summaries",
+content:updatedContent,
+sha:file.sha
+})
+})
 
 return {
 statusCode:200,
-body:"saved"
-}
-
-} catch(err){
-
-return {
-statusCode:500,
-body:"error"
-}
-
+body:"updated"
 }
 
 }
