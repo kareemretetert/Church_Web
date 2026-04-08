829fcea87df4267428c49e78bbb9cc783f931ad2
Kareem Hosny <hsnykrym27@gmail.com>
2026-04-08 16:59:08 +0200

    Update particles.js

--- javescriptfiles/particles.js
+++ javescriptfiles/particles.js
@@ -1,1 +1,22 @@
+// particles background
 
+function createParticles(){
+
+const container = document.createElement("div");
+container.className = "particles";
+document.body.appendChild(container);
+
+for(let i=0;i<25;i++){
+
+const p = document.createElement("span");
+
+p.style.left = Math.random()*100+"%";
+p.style.animationDelay = Math.random()*10+"s";
+p.style.animationDuration = 10 + Math.random()*10+"s";
+
+container.appendChild(p);
+}
+
+}
+
+window.addEventListener("load",createParticles);
